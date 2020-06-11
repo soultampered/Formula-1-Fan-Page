fa4f6cf628ad010fc23c6f71f4fac1550168e2d1
soultampered <42450113+soultampered@users.noreply.github.com>
2020-06-11 16:38:45 -0400

    form changes

--- FormValidity.js
+++ FormValidity.js
@@ -1,10 +1,33 @@
-//Checks form validity -wip-
-
-
-function validateName() {
-
-  var formValidate = document.getElementById(id1)
+//Checks form validity
 
 
 
-}
+
+  document.mainForm.myname.onfocus=function() {
+    document.getElementById("mynamehint").innerHTML = "Enter your last name, then first";
+  }
+
+  document.mainForm.myname.onblur=function() {
+    document.getElementById("mynamehint").innerHTML = "";
+ 
+  } //Name hint
+
+
+
+
+
+
+
+
+document.mainForm.email.onchange=function()  {
+  if (document.mainForm.email.value==="") {
+    document.getElementById("formerror").innerHTML = "Email address is required";
+  }
+
+document.mainForm.email.onchange=function()  {
+  var noName = document.mainForm.email.value;
+
+  if (noName.indexOf("@")) {
+  document.getElementById("formerror").innerHTML = "Email address format invalid";
+  }
+} //Validation Error - Email Address
