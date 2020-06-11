bb10465293f6b5bf219a1e42fdd45ef6bb49ccfb
soultampered <42450113+soultampered@users.noreply.github.com>
2020-06-10 23:34:48 -0400

    Form changes

--- FormValidity.js
+++ FormValidity.js
@@ -1,31 +1,10 @@
 //Checks form validity -wip-
 
 
-function validateName()
-{
-  var firstName = document.getElementById(id1);
-  var lastName = document.getElementById(id1);
+function validateName() {
 
-  if (firstName.validity.valueMissing, lastName.validity.valueMissing){    //if there is no data in the form
-  
-    input.setCustomValidtiy("Please enter your name");
-    input.reportValidtiy();
-  }
+  var formValidate = document.getElementById(id1)
 
-  else if (firstName.value.length > minlength, lastName.value.length > minlength) {  //if the text field is less than 2 character
-    input.setCustomValidtiy("Names must be a minimum 2 characters");
-    input.reportValidtiy();
-  }
 
-  else  {
-    input.setCustomValidtiy("");
-    input.reportValidtiy();
-  }
 
 }
-
-
-// function validateGuests(){
-
-
-// }
