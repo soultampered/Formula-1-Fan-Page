108de3e96fb7572b1a9edb95500dd4d9b0c81ec9
soultampered <42450113+soultampered@users.noreply.github.com>
2020-07-21 21:53:47 -0400

    validation changes
    added additional form validation

--- FormValidity.js
+++ FormValidity.js
@@ -13,7 +13,7 @@ for (key in inputFields) {
       var isValid = this.value.search(myPattern) >= 0;
 
       if (!(isValid)) {
-        formError.innerHTML = "Input does not match expected pattern. " + myPlaceholder;  //is not valid
+        formError.innerHTML = "Please enter the correct input. " + myPlaceholder;  //is not valid
       } else {
         formError.innerHTML = ""; //is valid
       }
@@ -21,41 +21,3 @@ for (key in inputFields) {
     }
 
 }
-
-
-// // Gives the user a hint for how to fill out their name in the name field
-// document.mainForm.myname.onfocus=function() {
-//     document.getElementById("mynamehint").innerHTML = "Enter your last name, then first";
-// }
-
-// document.mainForm.myname.onblur=function() {
-//     document.getElementById("mynamehint").innerHTML = "";
-// } //Name hint
-
-
-// // Checks if there is a date of birth filled out
-// document.mainForm.date.onchange=function()  {
-//   var da te = document.mainForm.date.value;
-
-//   if (date==="") {
-//     document.getElementById("dateError").innerHTML = "You must enter your birthdate";
-//   }
-// } //Validation Error - date
-
-
-
-
-// //Checks if there is an email address, and also if the email address is in a valid format
-// document.mainForm.email.onblur=function()  {
-//   var noEmail = document.mainForm.email.value;
-//   var emailError = document.getElementById('formerror');
-
-//   if (noEmail.indexOf("@") && noEmail.indexOf(".com") === -1) {
-//     emailError.innerHTML = "Invalid Email format!";
-  //   } else if (noEmail === "") {
-  //     emailError.innerHTML = "Email Address is required!";
-  //   } else {
-
-//     emailError.innerHTML = "";
-//   }
-// }
