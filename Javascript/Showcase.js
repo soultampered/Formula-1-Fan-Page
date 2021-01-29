8d084235667bb95eacd07e2fe142d47c23f760d9
soultampered <42450113+soultampered@users.noreply.github.com>
2021-01-29 00:10:59 -0500

    flags/venues
    added flags and venues

---
+++ Javascript/Showcase.js
@@ -0,0 +1,257 @@
+//List of Showcased Drivers
+var drivers = [
+		{name:"Fernando Alonso", //
+		picture:"Media/Portraits/Albon_portrait.png",
+		link:"https://www.formula1.com/en/drivers/alexander-albon.html"
+		},
+
+		{name:"Valtteri Bottas",
+		picture:"Media/Portraits/Bottas_portrait.png",
+		link:"https://www.formula1.com/en/drivers/valtteri-bottas.html"
+		},
+
+		{name:"Pierre Gasly",
+		picture:"Media/Portraits/Gasly_portrait.png",
+		link:"https://www.formula1.com/en/drivers/pierre-gasly.html"
+		},
+
+		{name:"Antonino Giovinazzi",
+		picture:"Media/Portraits/Giovinazzi_portrait.png",
+		link:"https://www.formula1.com/en/drivers/antonio-giovinazzi.html"
+		},
+
+		{name:"", //Nikita Mazipan
+		picture:"Media/Portraits/Grosjean_portrait.png",
+		link:"https://www.formula1.com/en/drivers/romain-grosjean.html"
+		},
+
+		{name:"Lewis Hamilton",
+		picture:"Media/Portraits/Hamilton_portrait.png",
+		link:"https://www.formula1.com/en/drivers/lewis-hamilton.html"
+		},
+
+		{name:"", //Yuki Tsnonda
+		picture:"Media/Portraits/Kvyat_portrait.png",
+		link:"https://www.formula1.com/en/drivers/daniil-kvyat.html"
+		},
+
+		{name:"Nicholas Latifi",
+		picture:"Media/Portraits/Latifi_portrait.png",
+		link:"https://www.formula1.com/en/drivers/nicholas-latifi.html"
+		},
+
+		{name:"Charles Leclerc",
+		picture:"Media/Portraits/Leclerc_portrait.png",
+		link:"https://www.formula1.com/en/drivers/charles-leclerc.html"
+		},
+
+		{name:"", //Mick schumacher
+		picture:"Media/Portraits/Magnussen_portrait.png",
+		link:"https://www.formula1.com/en/drivers/kevin-magnussen.html"
+		},
+
+		{name:"Lando Norris",
+		 picture:"Media/Portraits/Norris_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/lando-norris.html"
+		},
+
+		{name:"Esteban Ocon",
+		 picture:"Media/Portraits/Ocon_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/esteban-ocon.html"
+		},
+
+		{name:"Sergio Perez",
+		 picture:"Media/Portraits/Perez_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/sergio-perez.html"
+		},
+
+		{name:"Kimi Raikkonen",
+		 picture:"Media/Portraits/Raikkonen_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/kimi-raikkonen.html"
+		},
+
+		{name:"Daniel Ricciardo",
+		 picture:"Media/Portraits/Ricciardo_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/daniel-ricciardo.html"
+		},
+
+		{name:"George Russell",
+		 picture:"Media/Portraits/Russell_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/george-russell.html"
+		},
+
+		{name:"Carlos Sainz",
+		 picture:"Media/Portraits/Sainz_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/carlos-sainz.html"
+		},
+
+		{name:"Lance Stroll",
+		 picture:"Media/Portraits/Stroll_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/lance-stroll.html"
+		},
+
+		{name:"Max Verstappen",
+		 picture:"Media/Portraits/Verstappen_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/max-verstappen.html"
+		},
+
+		{name:"Sebastian Vettel",
+		 picture:"Media/Portraits/Vettel_portrait.png",
+		 link:"https://www.formula1.com/en/drivers/sebastian-vettel.html"
+		}
+];
+
+//List of Showcased Venues
+var venues = [
+    {
+        venue_RaceTitle: "Bahrain",
+        venue_flag: "Media/World_Flags/flagBahrain.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "San_Marino",
+        venue_flag: "Media/World_Flags/flagItaly.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {    
+        venue_name: "China",
+        venue_flag: "Media/World_Flags/flagChina.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Spain",
+        venue_flag: "Media/World_Flags/flagSpain.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Monaco",
+        venue_flag: "Media/World_Flags/flagMonaco.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Azerbaijan",
+        venue_flag: "Media/World_Flags/flagAzerbaijan.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Canada",
+        venue_flag: "Media/World_Flags/flagCanada.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "France",
+        venue_flag: "Media/World_Flags/flagFrance.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Austria",
+        venue_flag: "Media/World_Flags/flagAustria.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Britain",
+        venue_flag: "Media/World_Flags/flagUnitedKingdom.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Hungary",
+        venue_flag: "Media/World_Flags/flagHungary.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Belgium",
+        venue_flag: "Media/World_Flags/flagBelgium.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Netherlands",
+        venue_flag: "Media/World_Flags/flagNetherlands.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Italy",
+        venue_flag: "Media/World_Flags/flagItaly.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Russia",
+        venue_flag: "Media/World_Flags/flagRussia.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Singapore",
+        venue_flag: "Media/World_Flags/flagSingapore.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Japan",
+        venue_flag: "Media/World_Flags/flagJapan.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "UnitedStates",
+        venue_flag: "Media/World_Flags/flagUnitedStates.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Mexico",
+        venue_flag: "Media/World_Flags/flagMexico.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Brazil",
+        venue_flag: "Media/World_Flags/flagBrazil.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Australia",
+        venue_flag: "Media/World_Flags/flagAustralia.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Saudi Arabia",
+        venue_flag: "Media/World_Flags/flagSaudiArabia.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        },
+    {
+        venue_name: "Abu Dhabi",
+        venue_flag: "Media/World_Flags/flagAbuDhabi.jpeg",
+        venue_link: "",
+        venue_raceDate: ""
+        }
+
+];
+
+//Picks a randomly selected driver from the above list
+function replaceDriverSrc() {
+    var randomPick = Math.floor(Math.random() * Math.floor(drivers.length));
+    var randomPortrait = drivers[randomPick];
+    
+	document.getElementById("driverImage").src = randomPortrait.picture;
+	document.getElementById("driverLink").href = randomPortrait.link;
+	document.getElementById("driverName").innerHTML = randomPortrait.name;
+}
+
+window.onload = replaceDriverSrc();
