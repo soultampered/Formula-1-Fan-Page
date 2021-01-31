8cc340dd4900f77bf77c5e19b6dab06b0c909273
soultampered <42450113+soultampered@users.noreply.github.com>
2021-01-31 07:38:50 -0500

    Update Showcase.js

--- Javascript/Showcase.js
+++ Javascript/Showcase.js
@@ -106,140 +106,140 @@ var venues = [
     {
         venue_RaceTitle: "Bahrain",
         venue_flag: "Media/World_Flags/flagBahrain.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Bahrain.html",
+        venue_raceDate: "2021-03-28"
         },
     {
         venue_name: "San_Marino",
         venue_flag: "Media/World_Flags/flagItaly.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Imola.html",
+        venue_raceDate: "2021-04-18"
         },
     {    
         venue_name: "China",
         venue_flag: "Media/World_Flags/flagChina.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/China.html",
+        venue_raceDate: "2021-05-02"
         },
     {
         venue_name: "Spain",
         venue_flag: "Media/World_Flags/flagSpain.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Spain.html",
+        venue_raceDate: "2021-05-09"
         },
     {
         venue_name: "Monaco",
         venue_flag: "Media/World_Flags/flagMonaco.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Monaco.html",
+        venue_raceDate: "2021-05-23"
         },
     {
         venue_name: "Azerbaijan",
         venue_flag: "Media/World_Flags/flagAzerbaijan.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Azerbaijan.html",
+        venue_raceDate: "2021-06-06"
         },
     {
         venue_name: "Canada",
         venue_flag: "Media/World_Flags/flagCanada.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Canada.html",
+        venue_raceDate: "2021-06-13"
         },
     {
         venue_name: "France",
         venue_flag: "Media/World_Flags/flagFrance.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/France.html",
+        venue_raceDate: "2021-06-27"
         },
     {
         venue_name: "Austria",
         venue_flag: "Media/World_Flags/flagAustria.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Austria.html",
+        venue_raceDate: "2021-07-04"
         },
     {
         venue_name: "Britain",
         venue_flag: "Media/World_Flags/flagUnitedKingdom.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Great_Britain.html",
+        venue_raceDate: "2021-07-18"
         },
     {
         venue_name: "Hungary",
         venue_flag: "Media/World_Flags/flagHungary.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Hungary.html",
+        venue_raceDate: "2021-08-01"
         },
     {
         venue_name: "Belgium",
         venue_flag: "Media/World_Flags/flagBelgium.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Belgium.html",
+        venue_raceDate: "2021-08-29"
         },
     {
         venue_name: "Netherlands",
         venue_flag: "Media/World_Flags/flagNetherlands.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Netherlands.html",
+        venue_raceDate: "2021-09-05"
         },
     {
         venue_name: "Italy",
         venue_flag: "Media/World_Flags/flagItaly.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Italy.html",
+        venue_raceDate: "2021-09-12"
         },
     {
         venue_name: "Russia",
         venue_flag: "Media/World_Flags/flagRussia.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Russia.html",
+        venue_raceDate: "2021-09-26"
         },
     {
         venue_name: "Singapore",
         venue_flag: "Media/World_Flags/flagSingapore.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Singapore.html",
+        venue_raceDate: "2021-10-03"
         },
     {
         venue_name: "Japan",
         venue_flag: "Media/World_Flags/flagJapan.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Japan.html",
+        venue_raceDate: "2021-10-10"
         },
     {
         venue_name: "UnitedStates",
         venue_flag: "Media/World_Flags/flagUnitedStates.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/United_States.html",
+        venue_raceDate: "2021-10-24"
         },
     {
         venue_name: "Mexico",
         venue_flag: "Media/World_Flags/flagMexico.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Mexico.html",
+        venue_raceDate: "2021-10-31"
         },
     {
         venue_name: "Brazil",
         venue_flag: "Media/World_Flags/flagBrazil.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Brazil.html",
+        venue_raceDate: "2021-11-07"
         },
     {
         venue_name: "Australia",
         venue_flag: "Media/World_Flags/flagAustralia.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Australia.html",
+        venue_raceDate: "2021-11-21"
         },
     {
         venue_name: "Saudi Arabia",
         venue_flag: "Media/World_Flags/flagSaudiArabia.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/Saudi_Arabia.html",
+        venue_raceDate: "2021-12-05"
         },
     {
         venue_name: "Abu Dhabi",
         venue_flag: "Media/World_Flags/flagAbuDhabi.jpeg",
-        venue_link: "",
-        venue_raceDate: ""
+        venue_link: "https://www.formula1.com/en/racing/2021/United_Arab_Emirates.html",
+        venue_raceDate: "2021-12-12"
         }
 
 ];
