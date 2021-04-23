4587f5f17839984e5fd065b001fe3098d6b196d2
soultampered <42450113+soultampered@users.noreply.github.com>
2021-04-22 22:27:12 -0400

    showcase js changes

--- Javascript/Showcase.js
+++ Javascript/Showcase.js
@@ -1,182 +1,182 @@
 var drivers = [//List of Showcased Drivers
 	{
-    name: 'Fernando Alonso',
-    age: 39,
-    team: 'Alpine',
-    nationality: 'Spanish',
-	picture: 'Media/Portraits/Albon_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/fernando-alonso.html'
+        name: 'Fernando Alonso',
+        age: 39,
+        team: 'Alpine',
+        nationality: 'Spanish',
+    	picture: 'Media/Portraits/Albon_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/fernando-alonso.html'
 	},
 
 	{
-    name: 'Valtteri Bottas',
-    age: 31,
-    team: 'Mercedes',
-    nationality: 'Finnish',
-	picture: 'Media/Portraits/Bottas_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/valtteri-bottas.html'
+        name: 'Valtteri Bottas',
+        age: 31,
+        team: 'Mercedes',
+        nationality: 'Finnish',
+    	picture: 'Media/Portraits/Bottas_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/valtteri-bottas.html'
 	},
 
 	{
-    name: 'Pierre Gasly',
-    age: 25,
-    team: 'Alpha Tauri',
-    nationality: 'French',
-	picture: 'Media/Portraits/Gasly_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/pierre-gasly.html'
+        name: 'Pierre Gasly',
+        age: 25,
+        team: 'Alpha Tauri',
+        nationality: 'French',
+    	picture: 'Media/Portraits/Gasly_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/pierre-gasly.html'
 	},
 
 	{
-    name: 'Antonino Giovinazzi',
-    age: 27,
-    team: 'Alfa Romeo',
-    nationality: 'Italian',
-	picture: 'Media/Portraits/Giovinazzi_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/antonio-giovinazzi.html'
+        name: 'Antonino Giovinazzi',
+        age: 27,
+        team: 'Alfa Romeo',
+        nationality: 'Italian',
+    	picture: 'Media/Portraits/Giovinazzi_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/antonio-giovinazzi.html'
 	},
 
 	{
-    name: 'Nikita Mazipan',
-    age: 22,
-    team: 'Haas',
-    nationality: 'Russian',
-	picture: 'Media/Portraits/Grosjean_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/nikita-mazipan.html'
+        name: 'Nikita Mazipan',
+        age: 22,
+        team: 'Haas',
+        nationality: 'Russian',
+    	picture: 'Media/Portraits/Grosjean_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/nikita-mazipan.html'
 	},
 
 	{
-    name: 'Lewis Hamilton',
-    age: 36,
-    team: 'Mercedes',
-    nationality: 'British',
-	picture: 'Media/Portraits/Hamilton_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/lewis-hamilton.html'
+        name: 'Lewis Hamilton',
+        age: 36,
+        team: 'Mercedes',
+        nationality: 'British',
+    	picture: 'Media/Portraits/Hamilton_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/lewis-hamilton.html'
 	},
 
 	{
-    name: 'Yuki Tsunoda',
-    age: 20,
-    team: 'Alpha Tauri',
-    nationality: 'Japanese',
-	picture: 'Media/Portraits/Kvyat_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/Yuki-Tsunoda.html'
+        name: 'Yuki Tsunoda',
+        age: 20,
+        team: 'Alpha Tauri',
+        nationality: 'Japanese',
+    	picture: 'Media/Portraits/Kvyat_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/Yuki-Tsunoda.html'
 	},
 
 	{
-    name: 'Nicholas Latifi',
-    age: 25,
-    team: 'Williams',
-    nationality: 'Canadian',
-	picture: 'Media/Portraits/Latifi_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/nicholas-latifi.html'
+        name: 'Nicholas Latifi',
+        age: 25,
+        team: 'Williams',
+        nationality: 'Canadian',
+    	picture: 'Media/Portraits/Latifi_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/nicholas-latifi.html'
 	},
 
 	{
-    name: 'Charles Leclerc',
-    age: 23,
-    team: 'Ferrari',
-    nationality: 'Monagasque',
-	picture: 'Media/Portraits/Leclerc_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/charles-leclerc.html'
+        name: 'Charles Leclerc',
+        age: 23,
+        team: 'Ferrari',
+        nationality: 'Monagasque',
+    	picture: 'Media/Portraits/Leclerc_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/charles-leclerc.html'
 	},
 
 	{
-    name: 'Mick Schumacher',
-    age: 22,
-    team: 'Haas',
-    nationality: 'German',
-	picture: 'Media/Portraits/Magnussen_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/mick-schumacher.html'
+        name: 'Mick Schumacher',
+        age: 22,
+        team: 'Haas',
+        nationality: 'German',
+    	picture: 'Media/Portraits/Magnussen_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/mick-schumacher.html'
 	},
 
 	{
-    name: 'Lando Norris',
-    age: 21,
-    team: 'Mclaren',
-    nationality: 'British',
-	picture: 'Media/Portraits/Norris_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/lando-norris.html'
+        name: 'Lando Norris',
+        age: 21,
+        team: 'Mclaren',
+        nationality: 'British',
+    	picture: 'Media/Portraits/Norris_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/lando-norris.html'
 	},
 
 	{
-    name: 'Esteban Ocon',
-    age: 24,
-    team: 'Alpine',
-    nationality: 'French',
-	picture: 'Media/Portraits/Ocon_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/esteban-ocon.html'
+        name: 'Esteban Ocon',
+        age: 24,
+        team: 'Alpine',
+        nationality: 'French',
+    	picture: 'Media/Portraits/Ocon_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/esteban-ocon.html'
 	},
 
 	{
-    name: 'Sergio Perez',
-    age: 31,
-    team: 'Red Bull',
-    nationality: 'Mexican',
-	picture: 'Media/Portraits/Perez_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/sergio-perez.html'
+        name: 'Sergio Perez',
+        age: 31,
+        team: 'Red Bull',
+        nationality: 'Mexican',
+    	picture: 'Media/Portraits/Perez_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/sergio-perez.html'
 	},
 
 	{
-    name: 'Kimi Raikkonen',
-    age: 41,
-    team: 'Alfa Romeo',
-    nationality: 'Finnish',
-	picture: 'Media/Portraits/Raikkonen_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/kimi-raikkonen.html'
+        name: 'Kimi Raikkonen',
+        age: 41,
+        team: 'Alfa Romeo',
+        nationality: 'Finnish',
+    	picture: 'Media/Portraits/Raikkonen_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/kimi-raikkonen.html'
 	},
 
 	{
-    name: 'Daniel Ricciardo',
-    age: 31,
-    team: 'Mclaren',
-    nationality: 'Australian',
-	picture: 'Media/Portraits/Ricciardo_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/daniel-ricciardo.html'
+        name: 'Daniel Ricciardo',
+        age: 31,
+        team: 'Mclaren',
+        nationality: 'Australian',
+    	picture: 'Media/Portraits/Ricciardo_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/daniel-ricciardo.html'
 	},
 
 	{
-    name: 'George Russell',
-    age: 23,
-    team: 'Williams',
-    nationality: 'British',
-	picture: 'Media/Portraits/Russell_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/george-russell.html'
+        name: 'George Russell',
+        age: 23,
+        team: 'Williams',
+        nationality: 'British',
+    	picture: 'Media/Portraits/Russell_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/george-russell.html'
 	},
 
 	{
-    name: 'Carlos Sainz',
-    age: 26,
-    team: 'Ferrari',
-    nationality: 'Spanish',
-	picture: 'Media/Portraits/Sainz_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/carlos-sainz.html'
+        name: 'Carlos Sainz',
+        age: 26,
+        team: 'Ferrari',
+        nationality: 'Spanish',
+    	picture: 'Media/Portraits/Sainz_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/carlos-sainz.html'
 	},
 
 	{
-    name: 'Lance Stroll',
-    age: 22,
-    team: 'Aston Martin',
-    nationality: 'Canadian',
-	picture: 'Media/Portraits/Stroll_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/lance-stroll.html'
+        name: 'Lance Stroll',
+        age: 22,
+        team: 'Aston Martin',
+        nationality: 'Canadian',
+    	picture: 'Media/Portraits/Stroll_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/lance-stroll.html'
 	},
 
 	{
-    name: 'Max Verstappen',
-    age: 23,
-    team: 'Red Bull',
-    nationality: 'Dutch',
-	picture: 'Media/Portraits/Verstappen_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/max-verstappen.html'
+        name: 'Max Verstappen',
+        age: 23,
+        team: 'Red Bull',
+        nationality: 'Dutch',
+    	picture: 'Media/Portraits/Verstappen_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/max-verstappen.html'
 	},
 
 	{
-    name: 'Sebastian Vettel',
-    age: 33,
-    team: 'Aston Martin',
-    nationality: 'German',
-	picture: 'Media/Portraits/Vettel_portrait.png',
-	link: 'https://www.formula1.com/en/drivers/sebastian-vettel.html'
+        name: 'Sebastian Vettel',
+        age: 33,
+        team: 'Aston Martin',
+        nationality: 'German',
+    	picture: 'Media/Portraits/Vettel_portrait.png',
+    	link: 'https://www.formula1.com/en/drivers/sebastian-vettel.html'
 	}
 ];
 var venues = [//List of Showcased Venues
@@ -184,140 +184,231 @@ var venues = [//List of Showcased Venues
         raceTitle: "Bahrain",
         flag: "Media/World_Flags/flagBahrain.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Bahrain.html",
-        raceDate: new Date("2021-03-28")
-        },
+        raceDate: new Date("2021-03-28"),
+        lapCount: 57,
+        lapDistance: 5.412,
+        cornerCount: 15
+    },
+
     {
         raceTitle: "San_Marino",
         flag: "Media/World_Flags/flagItaly.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Imola.html",
-        raceDate: new Date("2021-04-18")
-        },
+        raceDate: new Date("2021-04-18"),
+        lapCount: 63,
+        lapDistance: 4.909, 
+        cornerCount: 19
+    },
+
     {    
-        raceTitle: "China",
+        raceTitle: "Portugal",
         flag: "Media/World_Flags/flagChina.jpeg",
-        link: "https://www.formula1.com/en/racing/2021/China.html",
-        raceDate: new Date("2021-05-02")
-        },
+        link: "https://www.formula1.com/en/racing/2021/Portugal.html",
+        raceDate: new Date("2021-05-02"),
+        lapCount: 66,
+        lapDistance: 4.653,
+        cornerCount: 15
+    },
+
     {
         raceTitle: "Spain",
         flag: "Media/World_Flags/flagSpain.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Spain.html",
-        raceDate: new Date("2021-05-09")
-        },
+        raceDate: new Date("2021-05-09"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Monaco",
         flag: "Media/World_Flags/flagMonaco.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Monaco.html",
-        raceDate: new Date("2021-05-23")
-        },
+        raceDate: new Date("2021-05-23"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Azerbaijan",
         flag: "Media/World_Flags/flagAzerbaijan.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Azerbaijan.html",
-        raceDate: new Date("2021-06-14")
-        },
+        raceDate: new Date("2021-06-14"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Canada",
         flag: "Media/World_Flags/flagCanada.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Canada.html",
-        raceDate: new Date("2021-06-13")
-        },
+        raceDate: new Date("2021-06-13"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "France",
         flag: "Media/World_Flags/flagFrance.jpeg",
         link: "https://www.formula1.com/en/racing/2021/France.html",
-        raceDate: new Date("2021-06-27")
-        },
+        raceDate: new Date("2021-06-27"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Austria",
         flag: "Media/World_Flags/flagAustria.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Austria.html",
-        raceDate: new Date("2021-07-04")
-        },
+        raceDate: new Date("2021-07-04"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Britain",
         flag: "Media/World_Flags/flagUnitedKingdom.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Great_Britain.html",
-        raceDate: new Date("2021-07-18")
-        },
+        raceDate: new Date("2021-07-18"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Hungary",
         flag: "Media/World_Flags/flagHungary.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Hungary.html",
-        raceDate: new Date("2021-08-01")
-        },
+        raceDate: new Date("2021-08-01"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Belgium",
         flag: "Media/World_Flags/flagBelgium.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Belgium.html",
-        raceDate: new Date("2021-08-29")
-        },
+        raceDate: new Date("2021-08-29"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Netherlands",
         flag: "Media/World_Flags/flagNetherlands.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Netherlands.html",
-        raceDate: new Date("2021-09-05")
-        },
+        raceDate: new Date("2021-09-05"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Italy",
         flag: "Media/World_Flags/flagItaly.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Italy.html",
-        raceDate: new Date("2021-09-12")
-        },
+        raceDate: new Date("2021-09-12"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Russia",
         flag: "Media/World_Flags/flagRussia.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Russia.html",
-        raceDate: new Date("2021-09-26")
-        },
+        raceDate: new Date("2021-09-26"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Singapore",
         flag: "Media/World_Flags/flagSingapore.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Singapore.html",
-        raceDate: new Date("2021-10-03")
-        },
+        raceDate: new Date("2021-10-03"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Japan",
         flag: "Media/World_Flags/flagJapan.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Japan.html",
-        raceDate: new Date("2021-10-09")
-        },
+        raceDate: new Date("2021-10-09"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "United States",
         flag: "Media/World_Flags/flagUnitedStates.jpeg",
         link: "https://www.formula1.com/en/racing/2021/United_States.html",
-        raceDate: new Date("2021-10-24")
-        },
+        raceDate: new Date("2021-10-24"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Mexico",
         flag: "Media/World_Flags/flagMexico.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Mexico.html",
-        raceDate: new Date("2021-10-31")
-        },
+        raceDate: new Date("2021-10-31"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Brazil",
         flag: "Media/World_Flags/flagBrazil.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Brazil.html",
-        raceDate: new Date("2021-11-07")
-        },
+        raceDate: new Date("2021-11-07"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Australia",
         flag: "Media/World_Flags/flagAustralia.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Australia.html",
-        raceDate: new Date("2021-11-21")
-        },
+        raceDate: new Date("2021-11-21"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Saudi Arabia",
         flag: "Media/World_Flags/flagSaudiArabia.jpeg",
         link: "https://www.formula1.com/en/racing/2021/Saudi_Arabia.html",
-        raceDate: new Date("2021-12-05")
-        },
+        raceDate: new Date("2021-12-05"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    },
+
     {
         raceTitle: "Abu Dhabi",
         flag: "Media/World_Flags/flagAbuDhabi.jpeg",
         link: "https://www.formula1.com/en/racing/2021/United_Arab_Emirates.html",
-        raceDate: new Date("2021-12-11")
-        }
+        raceDate: new Date("2021-12-11"),
+        lapCount: 'placeholder',
+        lapDistance: 'placeholder',
+        cornerCount: 'placeholder'
+    }
 ];
 var endOfSeason = { //Default Event Showcase display
         raceTitle: "The Season has finished",
@@ -344,10 +435,12 @@ function nextRace() {
         for (var i = 0; i <= (venues.length-1); i++) {
             var raceDate = venues[i].raceDate 
                 if (currentTime <= raceDate.getTime()) {
-                    document.getElementById("raceFlag").src = venues[i].flag;
-                    document.getElementById("raceLink").href = venues[i].link;
-                    document.getElementById("raceName").innerHTML = "Country: " + venues[i].raceTitle;
-                    console.log(venues[i].raceTitle);
+                    document.getElementById('raceFlag').src = venues[i].flag;
+                    document.getElementById('raceLink').href = venues[i].link;
+                    document.getElementById('raceName').innerHTML = "Country: " + venues[i].raceTitle;
+                    document.getElementById('lapCount').innerHTML = venues[i].lapCount;
+                    document.getElementById('lapDistance').innerHTML = venues[i].lapDistance + ' km';
+                    document.getElementById('cornerCount').innerHTML = venues[i].cornerCount;
                     break;
                 } else {                                 
                     document.getElementById("raceFlag").src = endOfSeason.flag;
