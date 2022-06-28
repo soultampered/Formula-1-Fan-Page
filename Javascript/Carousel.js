629c29f43db1f55de225d2db7c411db2aaa8aa5b
soultampered <42450113+soultampered@users.noreply.github.com>
2022-06-27 23:28:12 -0400

    Showcase and carousel work

---
+++ Javascript/Carousel.js
@@ -0,0 +1,25 @@
+document.querySelectorAll(".carousel").forEach(carousel => {
+	const items = carousel.querySelectorAll(".carousel_item");
+	const buttonsHtml = Array.from(items, () => {
+		return `<span class="carousel_btn></span>`;
+	});
+
+	carousel.insertAdjacentHTML("beforeend",`
+		<div class="carousel_nav">
+			${buttonsHtml.join("")}
+		</div>
+		`);
+
+	const buttons = carousel.querySelectorAll("carousel_btn");
+
+	buttons.forEach((button, i) => {
+		button.addEventListener("click", () => {
+			//deselect all items
+			items.forEach(item => item.classList.remove("carousel_item--selected"));
+			buttons.forEach(button => button.classList.remove("carousel_btn--selected"));
+
+			items[i].classList.add("carousel_item--selected");
+			button[i].classList.add("carousel_btn--selected");
+		});
+	});
+});
