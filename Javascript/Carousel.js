e636c117a13696994fcc093ca42c92d1484eafcb
soultampered <42450113+soultampered@users.noreply.github.com>
2022-06-29 14:28:46 -0400

    Carousel Work

--- Javascript/Carousel.js
+++ Javascript/Carousel.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".carousel").forEach(carousel => {
 	const items = carousel.querySelectorAll(".carousel_item");
 	const buttonsHtml = Array.from(items, () => {
-		return `<span class="carousel_btn"></span>`;
+		return `<span class="carousel_pin"></span>`;
 	});
 
 	carousel.insertAdjacentHTML("beforeend", `
@@ -10,20 +10,19 @@ document.querySelectorAll(".carousel").forEach(carousel => {
 		</div>
 		`);
 
-	const buttons = carousel.querySelectorAll(".carousel_btn");
-	console.log(buttons);
+	const buttons = carousel.querySelectorAll(".carousel_pin");
 
 	buttons.forEach((button, i) => {
 		button.addEventListener("click", () => {
 			//deselect all items
 			items.forEach(item => item.classList.remove("carousel_item--selected"));
-			buttons.forEach(button => button.classList.remove("carousel_btn--selected"));
+			buttons.forEach(button => button.classList.remove("carousel_pin--selected"));
 
 			items[i].classList.add("carousel_item--selected");
-			button.classList.add("carousel_btn--selected");
+			button.classList.add("carousel_pin--selected");
 		});
 	});
 
 	items[0].classList.add("carousel_item--selected");
-	buttons[0].classList.add("carousel_btn--selected");
+	buttons[0].classList.add("carousel_pin--selected");
 });
